04378535a963818724f30d6de1c0ec1e684d40ec
sminAhn <smin.ahn.x@gmail.com>
2020-03-21 09:12:20 +0900

    testcase
    테스트케이스추가

---
+++ src/test/java/org/smin/init/initTest.java
@@ -0,0 +1,14 @@
+package org.smin.init;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class initTest {
+
+    @Test
+    public void systemOut() {
+        init i = new init();
+        i.systemOut();
+    }
+}
